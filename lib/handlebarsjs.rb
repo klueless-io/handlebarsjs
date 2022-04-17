7f290984f6d289d0d203d15aaa9f44e49c51a844
David Cruwys <david@ideasmen.com.au>
2022-04-18 09:17:31 +1000

    chore: opinionated gem files

--- lib/handlebarsjs.rb
+++ lib/handlebarsjs.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "handlebarsjs/version"
+require_relative 'handlebarsjs/version'
 
 module Handlebarsjs
-  class Error < StandardError; end
+  # raise Handlebarsjs::Error, 'Sample message'
+  Error = Class.new(StandardError)
+
   # Your code goes here...
+end
+
+if ENV['KLUE_DEBUG']&.to_s&.downcase == 'true'
+  namespace = 'Handlebarsjs::Version'
+  file_path = $LOADED_FEATURES.find { |f| f.include?('handlebarsjs/version') }
+  version   = Handlebarsjs::VERSION.ljust(9)
+  puts "#{namespace.ljust(35)} : #{version.ljust(9)} : #{file_path}"
 end
