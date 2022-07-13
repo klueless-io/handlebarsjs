006b69a061f5acd2d15e302c321730621cf5a212
David Cruwys <david@ideasmen.com.au>
2022-07-13 17:11:36 +1000

    fix: update rubocop.yml

--- spec/handlebarsjs/handlebars_spec.rb
+++ spec/handlebarsjs/handlebars_spec.rb
@@ -162,7 +162,7 @@ RSpec.describe Handlebarsjs::Handlebars do
         KConfig.reset
         expect(KConfig.configuration.handlebars.helpers.length).to eq(0)
         KConfig.configuration.handlebars.defaults.add_array_defaults
-        expect(KConfig.configuration.handlebars.helpers.length).to be > 0
+        expect(KConfig.configuration.handlebars.helpers.length).to be.positive?
       end
     end
 
@@ -171,7 +171,7 @@ RSpec.describe Handlebarsjs::Handlebars do
         KConfig.reset
         expect(KConfig.configuration.handlebars.helpers.length).to eq(0)
         KConfig.configuration.handlebars.defaults.add_case_defaults
-        expect(KConfig.configuration.handlebars.helpers.length).to be > 0
+        expect(KConfig.configuration.handlebars.helpers.length).to be.positive?
       end
     end
 
@@ -180,7 +180,7 @@ RSpec.describe Handlebarsjs::Handlebars do
         KConfig.reset
         expect(KConfig.configuration.handlebars.helpers.length).to eq(0)
         KConfig.configuration.handlebars.defaults.add_comparison_defaults
-        expect(KConfig.configuration.handlebars.helpers.length).to be > 0
+        expect(KConfig.configuration.handlebars.helpers.length).to be.positive?
       end
     end
 
@@ -189,7 +189,7 @@ RSpec.describe Handlebarsjs::Handlebars do
         KConfig.reset
         expect(KConfig.configuration.handlebars.helpers.length).to eq(0)
         KConfig.configuration.handlebars.defaults.add_inflection_defaults
-        expect(KConfig.configuration.handlebars.helpers.length).to be > 0
+        expect(KConfig.configuration.handlebars.helpers.length).to be.positive?
       end
     end
   end
